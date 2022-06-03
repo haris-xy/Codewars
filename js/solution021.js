902957293263490c1b55c22fab6d6be2e2c46065
Haris <harishusagicxy@gmail.com>
2022-06-03 20:38:58 +0200

    Solution021 done

---
+++ js/solution021.js
@@ -0,0 +1,36 @@
+/*
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+
+*/
+
+
+
+function basicOp(operation, value1, value2) {
+        let result;
+        if (operation == "+") {
+           result = value1 + value2
+        }
+        else if (operation == "-"){
+            result = value1 - value2
+        }
+        else if (operation == "*"){
+            result = value1 * value2
+        }
+        else if (operation == "/"){
+            result = value1 / value2
+        }
+        return result;
+    }
+
+
+    //OOOOOOORRR with Switch-Case
+    //OoooOOrrr
+
+    function basicOp(o, a, b) {
+        return eval(a+o+b);
+      }
