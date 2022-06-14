064786e01a4601bc9e3bfa7f0ba12c681761c269
Haris <harishusagicxy@gmail.com>
2022-06-14 22:15:58 +0200

    Solution033 shortest

--- js/solution033.js
+++ js/solution033.js
@@ -24,3 +24,7 @@ function countPositivesSumNegatives(input) {
 
 
       // OOOrr
+
+      function countPositivesSumNegatives(input) {
+        return input && input.length ? [input.filter(p => p > 0).length, input.filter(n => n < 0).reduce((a, b) => a + b, 0)] : [];
+    }
