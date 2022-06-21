06e00d4bc56b90754692d945ebdf825656588c05
Haris <harishusagicxy@gmail.com>
2022-06-21 13:05:11 +0200

    Solution040 update

--- js/solution040.js
+++ js/solution040.js
@@ -4,3 +4,6 @@ Write a function which calculates the average of the numbers in a given list.
 Note: Empty arrays should return 0.
 */
 
+function find_average(array) {
+    return newArray = array.reduce((a,b) => a + b, 0) / array.length  
+  }
