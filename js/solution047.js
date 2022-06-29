49f2e0995a8de3b154f2421e92587f3a4db2aad3
Haris <harishusagicxy@gmail.com>
2022-06-29 18:53:51 +0200

    Solution047 update

--- js/solution047.js
+++ js/solution047.js
@@ -7,4 +7,10 @@ function simpleMultiplication(number) {
       return number * 9
     }
     }
+
+    //Oorr
+
+function simpleMultiplication(number) {
+    return number % 2 == 0 ? number * 8 : number * 9
+    }
     
