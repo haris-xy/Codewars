f6746c32afec9a0629c45787657c60962df14a5a
Haris <harishusagicxy@gmail.com>
2022-07-01 18:56:27 +0200

    Solution048 update

--- js/solution048.js
+++ js/solution048.js
@@ -13,3 +13,9 @@ function betterThanAverage(classPoints, yourPoints) {
     let average = classPoints.reduce((a,b) => a + b) /classPoints.length;
     return average < yourPoints
   }
+
+  //Ooorr
+
+function betterThanAverage(classPoints, yourPoints) {
+    return yourPoints > classPoints.reduce((a, b) => a + b) / classPoints.length; 
+  }
