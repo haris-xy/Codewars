f2801a6421ab2f42c947b9909f5f8da4035dbcc6
Haris <harishusagicxy@gmail.com>
2022-07-17 21:54:36 +0200

    Solution055 update

--- js/solution055.js
+++ js/solution055.js
@@ -10,3 +10,15 @@ function rentalCarCost(d) {
           : d < 7 ? (d * 40) - 20
           : (d * 40) - 50
     }
+
+    //Ooor
+
+function rentalCarCost(d) {
+    if (d < 3){
+    return (d*40)
+    } 
+    else if (d < 7){
+    return (d*40) - 20
+    }
+    else return (d * 40) - 50
+    }
